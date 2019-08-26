70a35380e74a4fd5200c56ad65df2c54ea6a6de1
Jaskaranpreet Singh <jaskaranpreetsingh@Jaskaranpreets-MacBook-Pro.local>
2019-08-26 17:14:14 -0400

    Add high and low for forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,13 +2,13 @@ const request = require('request');
 
 const forecast = (latitude , longitude , callback) =>{
     const url = `https://api.darksky.net/forecast/87d21cdd094777a056439cc7fb4896ab/${longitude},${latitude}`;
-    request({ url , json:true }, (error, {body}={} )=>{
+    request({ url , json:true }, (error, {body} )=>{
         if (error){
             callback('Unable to connect to weather service!', undefined);
         }else if(body.error){
             callback(body.error,undefined)
         }else{
-            callback(undefined,`${body.daily.data[0].summary} It is currently ${body.currently.temperature} deggres out. There is a ${body.currently.precipProbability} % of rain`)
+            callback(undefined,`${body.daily.data[0].summary} It is currently ${body.currently.temperature} deggres out. The high today is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}. There is a ${body.currently.precipProbability} % of rain`)
         } 
     })
 }
